4d45791e7c15d6b512b3ac08d67aa94f7f26c33c
Mickaël Foucaux <mickael.foucaux@gmail.com>
2015-08-26 17:30:06 +0200

    add responsive and clean a bit the code

--- src/dang-jssor.js
+++ src/dang-jssor.js
@@ -1,5 +1,8 @@
-angular.module("dang-jssor", [])
-	.directive("enableJssor", ['$window', function($window) {
+'use strict';
+// widlely inspired by: https://github.com/adebisi-fa/dang-jssor
+
+angular.module('gdWebApp')
+	.directive("gdEnableJssor",  ['$window', function($window) {
 	    return {
 	        restrict: "A",
 	        scope: {
@@ -8,100 +11,104 @@ angular.module("dang-jssor", [])
 	            jssorObject: "="
 	        },
 	        link: function (scope, element, attrs) {
-	            if (attrs.jssorTrigger == 'true') {
-	                var container = angular.element(element).closest('.slides-container');
+            if (attrs.jssorTrigger == 'true') {
+              var container = angular.element(element).closest('.slides-container');
 
-	                if (!container.attr("id"))
-	                    container.attr("id", new Date().getTime());
+              if (!container.attr("id")) {
+                container.attr("id", new Date().getTime());
+              }
 
-	                if (scope.jssorOptions == undefined)
-	                {
-	                	console.log("I reassigned options here!");
-	                	scope.jssorOptions = {};
-	                }
+              if (scope.jssorOptions === undefined) {
+              	scope.jssorOptions = {};
+              }
 
-									var slideId = container.attr("id");
+              var slideId = container.attr("id");
 
-              		var slider = new $JssorSlider$(slideId, scope.jssorOptions);
+              var slider = new $JssorSlider$(slideId, scope.jssorOptions); // jshint ignore:line
 
-                	var handle = {
-                        slidesCount: slider.$SlidesCount(),
-                        slider: slider,
-                        playTo: function (index) {
-                            slider.$PlayTo(index);
-                        },
-                        goTo: function (index) {
-                            slider.$GoTo(index);
-                        },
-                        pause: function () {
-                            slider.$Pause();
-                        },
-                        play: function () {
-                            slider.$Play();
-                        },
-                        previous: function () {
-                            slider.$Previous();
-                        },
-                        next: function () {
-                            slider.$Next();
-                        }
-                    };
+            	var handle = {
+                  slidesCount: slider.$SlidesCount(),
+                  slider: slider,
+                  playTo: function (index) {
+                      slider.$PlayTo(index);
+                  },
+                  goTo: function (index) {
+                      slider.$GoTo(index);
+                  },
+                  pause: function () {
+                      slider.$Pause();
+                  },
+                  play: function () {
+                      slider.$Play();
+                  },
+                  previous: function () {
+                      slider.$Previous();
+                  },
+                  next: function () {
+                      slider.$Next();
+                  }
+                };
 
-                	if (scope.jssorOptions)
-                    	scope.jssorOptions.handle = handle;
+            	if (scope.jssorOptions) {
+                	scope.jssorOptions.handle = handle;
+								}
 
-	                if (scope.jssorObject)
-	                    scope.jssorObject = handle;
+              if (scope.jssorObject) {
+                  scope.jssorObject = handle;
+								}
 
-                	if (scope.jssorOptions.$StartIndex == undefined || scope.jssorOptions.$StartIndex == null)
-                		scope.jssorOptions.$StartIndex = 0;
+            	if (scope.jssorOptions.$StartIndex === undefined || scope.jssorOptions.$StartIndex === null) {
+            		scope.jssorOptions.$StartIndex = 0;
+              }
 
-                	handle.playTo(scope.jssorOptions.$StartIndex);
+            	handle.playTo(scope.jssorOptions.$StartIndex);
 
-	                if (scope.jssorOptions.onReady) {
-	                	scope.jssorOptions.onReady();
-	                };
+              if (scope.jssorOptions.onReady) {
+              	scope.jssorOptions.onReady();
+              }
 
-									var ScaleSlider = function() {
-	                  var parentWidth = $('#' + slideId).parent().width();
-	                  if (parentWidth) {
-	                    slider.$ScaleWidth(parentWidth);
-	                  }
-	                  else {
-	                    $window.setTimeout(ScaleSlider, 30);
+              var ScaleSlider = function() {
+                  var parentWidth = $('#' + slideId).parent().width();
+                  if (parentWidth) {
+                    slider.$ScaleWidth(parentWidth);
+                  }
+                  else {
+                    $window.setTimeout(ScaleSlider, 30);
+									}
+
+									angular.element(".img-slide").attr('style', ''); // remove dynamic style which was hidding 3rd and next images
+              };
+
+              //Scale slider after document ready
+              ScaleSlider();
+
+              //Scale slider while window load/resize/orientationchange.
+              angular.element($window).bind("load", ScaleSlider);
+							angular.element($window).bind("resize", ScaleSlider);
+							angular.element($window).bind("orientationchange", ScaleSlider);
+              //responsive code end
+
+              slider.$On($JssorSlider$. $EVT_PARK, function (slideIndex, fromIndex) {  // jshint ignore:line
+                  var status = null;
+
+                  scope.$emit("JssorSliderChanged", status = {
+                      name: scope.jssorOptions.name,
+                      slideIndex: slideIndex,
+                      fromIndex: fromIndex
+                  });
+
+                  if (scope.jssorOnChanged) {
+                      scope.jssorOnChanged({ jssorData: status });
 										}
 
-										angular.element(".img-slide").attr('style', ''); // remove dynamic style which was hidding 3rd and next images
-		              };
+                  scope.jssorOptions.status = status;
 
-		              //Scale slider after document ready
-		              ScaleSlider();
-
-		              //Scale slider while window load/resize/orientationchange.
-		              angular.element($window).bind("load", ScaleSlider);
-									angular.element($window).bind("resize", ScaleSlider);
-									angular.element($window).bind("orientationchange", ScaleSlider);
-
-	                slider.$On($JssorSlider$.$EVT_PARK, function (slideIndex, fromIndex) {
-	                    var status = null;
-
-	                    scope.$emit("JssorSliderChanged", status = {
-	                        name: scope.jssorOptions.name,
-	                        slideIndex: slideIndex,
-	                        fromIndex: fromIndex
-	                    });
-
-	                    if (scope.jssorOnChanged)
-	                        scope.jssorOnChanged({ jssorData: status });
-
-	                    scope.jssorOptions.status = status;
-
-	                    //if (scope.jssorOptions.name) {
-	                    //    console.log("SliderChanged:", scope.jssorOptions.name, angular.toJson(status));
-	                    //}
-	                    scope.$apply();
-	                });
-	            }
+                  if (scope.jssorOptions.name) {
+                      console.log("SliderChanged:", scope.jssorOptions.name, angular.toJson(status));
+                  }
+                  scope.$apply();
+              });
+            }
 	        }
-	    }
-		}]);
+	    };
+	}]);
